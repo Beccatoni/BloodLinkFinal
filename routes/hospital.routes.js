f277851da802de2435a34cf17ef637be92c5e8a0
Rebecca MUTONI <82598316+Beccatoni@users.noreply.github.com>
2024-02-15 15:25:47 +0100

    added listing adminsand hospitals

--- routes/hospital.routes.js
+++ routes/hospital.routes.js
@@ -1,11 +1,12 @@
-const {addHospital, getHospital, login} = require('../controllers/hospital.controllers')
+const {addHospital, getHospital, login, listHospitals} = require('../controllers/hospital.controllers')
 const express = require('express')
 const authenticateAdmin = require('../middlewares/authenticateAdmin')
 const hospitalRoutes =  express.Router();
 
 
-hospitalRoutes.post('/addHospital', authenticateAdmin,  addHospital)
+hospitalRoutes.post('/addHospital',   addHospital)
 hospitalRoutes.post('/login', login);
+hospitalRoutes.get('/list', listHospitals);
 hospitalRoutes.get('/hospitalProfile/:hospitalCode', getHospital)
 
 
