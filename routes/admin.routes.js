148e076858c4ce8924b3041908828bcf7f17f9b6
Rebecca MUTONI <82598316+Beccatoni@users.noreply.github.com>
2024-02-15 17:12:25 +0100

    add

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAdminProfile, getAllAdmins, generalLogin,} = require('../controllers/admin.controllers');
+const { getAdminProfile, getAllAdmins, generalLogin,AdminSignUp} = require('../controllers/admin.controllers');
 
 const adminRouter = express.Router();
 
+adminRouter.post('/signup', AdminSignUp);
 adminRouter.post('/login', generalLogin);
 adminRouter.get('/adminProfile/:adminId', getAdminProfile);
 adminRouter.get('/listallAdmins', getAllAdmins)
